b46a89f79379c850650b8656301c3183e05eac00
CQ <franciscor00@outlook.es>
2024-07-10 00:27:45 -0400

    Nueva version

---
+++ lib/template/app/client/Main.lua
@@ -0,0 +1,1 @@
+print('NoheJS Client-side')
---
+++ lib/template/view/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{ props.msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="Main.count++">count is {{ Main.count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Farm and Vue logos to learn more</p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Main from 'Store/Main'
+
+const props = defineProps(['msg'])
+const count = ref(0)
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
