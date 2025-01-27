70252a793092b1ab968fc2f0394f4e06cc5cf0ce
CQ <franciscor00@outlook.es>
2025-01-26 21:11:37 -0400

    Correccion en texto

--- view/components/HelloWorld.vue
+++ view/components/HelloWorld.vue
@@ -14,7 +14,7 @@
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the nohejs and Vue logos to learn more</p>
+  <p class="read-the-docs">Click on the Nohejs and Vue logos to learn more</p>
 </template>
 
 <script>
