82e4b628f02840ccb25bcb79b32d3bcb0a36671b
CQ <franciscor00@outlook.es>
2025-01-26 23:02:27 -0400

    Cambios en los archivos

--- view/main.jsx
+++ view/main.jsx
@@ -1,33 +1,7 @@
-import { useState } from 'react';
-import './main.css';
-import reactLogo from './assets/react.svg';
-import NoheLogo from './assets/nohe.png';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
-export function Main() {
-  const [count, setCount] = useState(0);
+const container = document.querySelector('#app');
+const root = createRoot(container);
 
-  return (
-    <>
-      <div>
-        <a href="/" target="_blank">
-          <img src={NoheLogo} className="logo" alt="Farm logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>NoheJS + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/main.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Nohejs and React logos to learn more
-      </p>
-    </>
-  );
-}
+root.render(<App />);
