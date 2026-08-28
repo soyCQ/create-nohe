ce9eab14c589ef21b6cb48bedd70bc712414369a
CQ <franciscor00@outlook.es>
2026-08-28 19:44:15 -0400

    NuevaVersion

--- view/counter.ts
+++ view/counter.ts
@@ -1,9 +1,11 @@
-export function setupCounter(element) {
+export function setupCounter(element: HTMLButtonElement): void {
   let counter = 0;
-  const setCounter = (count) => {
+  
+  const setCounter = (count: number): void => {
     counter = count;
     element.innerHTML = `count is ${counter}`;
   };
+
   element.addEventListener('click', () => setCounter(counter + 1));
   setCounter(0);
 }
